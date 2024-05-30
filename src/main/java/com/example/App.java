f4afc838e4985260258664df2b318e5a8d122c96
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-05-30 18:37:19 +0200

    first commit of Efe's guidelines

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,42 +1,28 @@
 package com.example;
-import java.util.Scanner;
 
 
 public final class App {
     private App() {
     }
 
-    
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
+        
+        IkeaFurniture furniture1 = new IkeaFurniture();
 
-       System.out.println("Please, select a type of furniture: ");
-       String type = sc.nextLine().toLowerCase();
-       System.out.println("You selected "+type);
+        furniture1.insertType();
+        System.out.println("You selected " + furniture1.type);
 
-       System.out.println("Please, select the material of your choice: ");
-       String material = sc.nextLine().toLowerCase();
-       System.out.println("You selected "+ material );
+        furniture1.insertMaterial();
+        System.out.println("You selected " + furniture1.material);
 
-       System.out.println("Please, select the color of your choice: ");
-       String color = sc.nextLine().toLowerCase();
-       System.out.println("You selected "+ color );
+        furniture1.insertColor();
+        System.out.println("You selected " + furniture1.color);
 
-       System.out.println("You are going to loose some screws (if you already didn't). How many extra screws do you need: ");
-       int numberScrews = sc.nextInt();
-       System.out.println("You selected "+ numberScrews + " extra screws.");
+        furniture1.insertExtraScrews();
+        System.out.println("You selected " + furniture1.extraScrews + " extra screws.");
 
-       /* System.out.println("Please, select the payment mode (cash or card): ");
-       String payment = sc.nextLine();
-       System.out.println("you selected"+ furnitureColor ); */
+        System.out.println(furniture1.toString());
 
-       sc.close();
-
-       IkeaFurniture furniture1 = new IkeaFurniture(type, material, color, numberScrews);
-
-       System.out.println("This is your order so far: \n" + "You want a "+furniture1.type +" made of " + furniture1.material + " in "+furniture1.color + " color " + "and you need "+ furniture1.extraScrews + " extra screws.\n" );
-
-
-
+        furniture1.closeScanner();
     }
 }
--- src/main/java/com/example/IkeaFurniture.java
+++ src/main/java/com/example/IkeaFurniture.java
@@ -1,24 +1,49 @@
 package com.example;
+
+import java.util.Scanner;
+
 public class IkeaFurniture {
-    
     String type;
-    String material; 
+    String material;
     String color;
-    int extraScrews; 
+    int extraScrews;
+    Scanner sc;
 
-
-    public IkeaFurniture(String type, String material, String color, int extraScrews) {
-        this.type = type;
-        this.material = material;
-        this.color = color;
-        this.extraScrews = extraScrews;
-
+    public IkeaFurniture() {
+        this.type = "";
+        this.material = "";
+        this.color = "";
+        this.extraScrews = 0;
+        this.sc = new Scanner(System.in);
     }
 
+    public void insertType() {
+        System.out.println("Please, select a type of furniture: ");
+        this.type = this.sc.nextLine().toLowerCase();
+    }
 
+    public void insertMaterial() {
+        System.out.println("Please, select the material of your choice: ");
+        this.material = this.sc.nextLine().toLowerCase();
+    }
 
-        
+    public void insertColor() {
+        System.out.println("Please, select the color of your choice: ");
+        this.color = this.sc.nextLine().toLowerCase();
+    }
+
+    public void insertExtraScrews() {
+        System.out.println("You are going to lose some screws (if you already didn't). How many extra screws do you need: ");
+        this.extraScrews = this.sc.nextInt();
+        sc.nextLine(); 
+    }
+    public void closeScanner() {
+        this.sc.close();
+    }
     
-
-    //public String 
+    public String toString() {
+        return "This is your order so far: \n" +
+               "You want a " + this.type + " made of " + this.material + " in " + this.color + " color " +
+               "and you need " + this.extraScrews + " extra screws.\n";
+    }
 }
